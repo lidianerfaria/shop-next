858b69d084685cf1d55ac1b79062fa62cabe18a8
Lidiane Faria <lidianerdefaria@gmail.com>
2024-02-26 22:35:34 -0300

    config: stitches framework

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import { styled } from '../styles';
+
+const Button = styled('button', {
+  backgroundColor: '$main',
+});
+
 export default function Home() {
-  return (
-    <>
-      <h1>Hello!</h1>
-    </>
-  );
+  return <Button>Hello!</Button>;
 }
---
+++ src/styles/index.ts
@@ -0,0 +1,18 @@
+import { createStitches } from '@stitches/react';
+
+export const {
+  config,
+  styled,
+  createTheme,
+  css,
+  getCssText,
+  globalCss,
+  keyframes,
+  theme,
+} = createStitches({
+  theme: {
+    colors: {
+      main: '#8257e6',
+    },
+  },
+});
