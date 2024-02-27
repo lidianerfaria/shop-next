f7ce0cc411382d58ed548ae53db8ed4cf6a4fb0b
Lidiane Faria <lidianerdefaria@gmail.com>
2024-02-27 16:40:52 -0300

    style: home structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
-import { styled } from '../styles';
-
-const Button = styled('button', {
-  backgroundColor: '$green500',
-});
+import camiseta1 from '../assets/camisetas/1.png';
+import camiseta2 from '../assets/camisetas/2.png';
+import camiseta3 from '../assets/camisetas/3.png';
+import { HomeContainer, Product } from '../styles/pages/home';
+import Image from 'next/image';
 
 export default function Home() {
-  return <Button>Hello!</Button>;
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt='' />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt='' />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  );
 }
